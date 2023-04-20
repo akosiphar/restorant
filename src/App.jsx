5c8f9e13bd9a623c3d88d9764e6788a79b6cf1bc
akosiphar <raphgalag@gmail.com>
2023-04-20 23:29:56 +0800

    added baseUrl

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
 
   return (
     <>
-    <BrowserRouter>
+    <BrowserRouter basename='/restorant'>
       <NavBar />
       <Routes>
         <Route path='/' element={<HomePage />} />
